1f03b7e1fd0950aa53ec7a9e722a3e023d03b6dd
berkan <berkan@nitrobilisim.com.tr>
2025-03-17 14:52:07 +0300

    CreateDiffModificationDto JSONB desteği ile oluşturuldu

---
+++ DecoderApi/DTOs/CreateDiffModificationDto.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DecoderApi.DTOs
+{
+    public class CreateDiffModificationDto
+    {
+        [Required]
+        public int TuneCatalogId { get; set; }
+        
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+        
+        [StringLength(255)]
+        public string Description { get; set; }
+        
+        [Required]
+        public long OffsetAddress { get; set; }
+        
+        [Required]
+        public string OriginalDataJson { get; set; }
+        
+        [Required]
+        public string ModifiedDataJson { get; set; }
+    }
+}
